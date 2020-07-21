56a2484b4db364800bae6b08bc1c16752d0daae3
phanphucan48 <phanphucan4890@gmail.com>
2020-07-21 10:34:13 +0700

    hoanthanh

---
+++ Module2-timkiem/TimKiem/TimKiem/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace TimKiem
+{
+    class Program
+    {
+        private static int[] array; 
+        static void Main(string[] args)
+        {
+
+            Timkiem timkiem = new Timkiem();
+            string choice = "";
+            while(choice != "5")
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Create Array");
+                Console.WriteLine("2. IsSymmetry Array");
+                Console.WriteLine("3. Selection Sort");
+                Console.WriteLine("4. Find Array");
+                Console.WriteLine("5. Exit ");
+                Console.WriteLine("Enter your choice: ");
+                choice = Console.ReadLine();
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("tao mang :");
+                        Console.WriteLine("input n:");
+                        int n = int.Parse(Console.ReadLine());
+                        array = timkiem.CreateArray(n);
+                        timkiem.PrintArray(array);
+                        break;
+                    case "2":
+                        Console.WriteLine("kiem tra mang doi xung ");
+                        if(timkiem.IsSymmetryArray(array)){
+                            Console.WriteLine("Mang doi xung");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mang Ko doi xung");
+                        }
+                        timkiem.PrintArray(array);
+                        break;
+                    case "3":
+                        Console.WriteLine("Sắp xếp mảng ");
+                        timkiem.SelectionSort(array);
+                        timkiem.PrintArray(array);
+                        break;
+                    case "4":
+                        Console.WriteLine("Tìm kiếm mảng: ");
+                        int number = int.Parse(Console.ReadLine());
+                        Console.WriteLine( timkiem.Find(array, number));
+                        timkiem.PrintArray(array);
+                        break;
+                    case "5":
+                        Console.WriteLine("Exit ");
+                        break;
+                }
+
+            }
+         
+        }
+        
+    }
+}
---
+++ Module2-timkiem/TimKiem/TimKiem/Timkiem.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimKiem
+{
+    public class Timkiem
+    {
+        public int[] CreateArray(int n)
+        {
+            Random random = new Random();
+            int[] result = new int[n];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = random.Next(30, 60);
+            }
+            return result;
+        }
+        public bool IsSymmetryArray(int[] array)
+        {
+
+            int first = 0, last = array.Length - 1;
+            while (first < last)
+            {
+                if (array[first] != array[last]) return false;
+                first++;
+                last--;
+            }
+            return true;
+        }
+        public void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        public int[] SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                double currentMin = array[i];
+                int currentMinIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (currentMin > array[j])
+                    {
+                        currentMin = array[j];
+                        currentMinIndex = j;
+                        int temp = array[currentMinIndex];
+                        array[currentMinIndex] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public bool IsSorted(int[] arr, int tanghoacgiam)
+        {
+            if (tanghoacgiam == 1)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] >= arr[i + 1])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else if (tanghoacgiam == -1)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] <= arr[i + 1])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        public string Find(int[] arr,int number)
+        {
+            if (IsSorted(arr,1) || IsSorted(arr,-1))
+            { // phuong thuc tim kiem nhi phan 
+                int low = 0;
+                int high = arr.Length - 1;
+                while (high > low)
+                {
+                    int mid = (high + low) / 2;
+                    if (number < arr[mid])
+                    {
+                        high = mid - 1;
+                    }
+                    else if (number == arr[mid])
+                    {
+                        return $"{mid}";
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                    return "Not found!";
+                }
+            } 
+            return "This array hasn't been sorted!";
+        }
+    }
+}
