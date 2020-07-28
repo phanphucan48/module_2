143511d2859a3a6d57d58b4406860d4a45459546
phanphucan48 <phanphucan4890@gmail.com>
2020-07-28 13:43:12 +0700

    hello

---
+++ Module2-File/File.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Module2_File
+{
+    public class FileName
+    {
+        public int[,] Matrix(int r, int c)
+        {
+            int[,] matrix = new int[r, c];
+
+            Random random = new Random();
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    matrix[i, j] = random.Next(10, 70);
+
+                }
+            }
+
+            return matrix;
+
+
+        }
+        public int SumMatrix(int[,] matrix)
+        {
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum += matrix[i, j];
+
+                }
+            }
+            return sum;
+        }
+        
+        public void ReadFile(string path, ref int[,] array)
+        {
+            List<string[]> list = new List<string[]>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                {
+                    var line = string.Empty;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(' ');
+                        list.Add(data);
+                    }
+                    array = new int[list.Count, list[0].Length];
+                    for(int i = 0; i < list.Count; i++)
+                    {
+                        for(int j = 0; j < list[i].Length; j++)
+                        {
+                            array[i, j] = Int32.Parse(list[i][j]);
+                        }
+                       
+                    }
+                }
+            }
+
+
+        }
+        public void NewWriteFile(string path, int[,] array)
+        {
+            using(StreamWriter sw = File.CreateText(path))
+            {
+                int sum = 0;
+                for(int i = 0; i < array.GetLength(0); i++)
+                {
+                    for(int j = 0; j < array.GetLength(1); j++)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+                sw.WriteLine(sum);
+                int countSNT = 0;
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if(CheckSNT(array[i, j]) == true) {
+                            countSNT++;
+                        }
+                    }
+                }
+                sw.WriteLine(countSNT);
+
+            }
+        }
+        public bool CheckSNT(int number)
+        {
+           
+            
+            if (number < 2)
+            {
+                return false;
+            }else if(number == 2)
+            {
+                return true;
+            }
+            else
+            {
+                for(int i = 2; i < number; i++)
+                {
+                    if(number % i == 0)
+                    {
+                        return false;
+                    }
+                }              
+                return true;
+            }
+        }
+       public int borderline(int [,] matrix) 
+        {
+            int sum = 0;
+            for ( int i = 0 ; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, 0] + matrix[i,matrix.GetLength(1)-1];
+            }
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[0, j] + matrix[matrix.GetLength(0)-1,j];
+            }
+            return sum; 
+        }
+        //public int productTheer(int [,] matrix)
+        //{
+        //    for (int i = 0; i < matrix.GetLength(0); i++)
+        //    {
+        //        for (int j = 0; j < matrix.GetLength(1); j++) 
+        //        { 
+
+        //        }
+        //    }
+        //}
+
+
+
+
+
+    }
+}
---
+++ Module2-File/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Module2_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileName newfile = new FileName();
+           // newfile.Matrix(4, 6);
+            //int[,] matrix = newfile.Matrix(4, 6);
+            int[,] array = new int[0, 0];
+            var input = $@"C:\Users\Admin\source\repos\Module2-File\Module2-File\Data\InputData.txt";
+            var output = @"C:\Users\Admin\source\repos\Module2-File\Module2-File\Data\OutputData.txt";
+            //newfile.ReadFile(input,ref array);
+            //newfile.NewWriteFile(output, array);
+            //newfile.
+            List<string[]> data = new List<string[]>();
+            using (StreamReader sr = File.OpenText(input))
+            {
+                //Console.WriteLine(sr.ReadToEnd());
+                var line = string.Empty;
+                int number = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    number += 1;
+                    //Console.WriteLine($"Line {number} : {line.ToUpper()}");
+                    data.Add(line.ToUpper().Split(" "));
+                }
+
+            }
+            Console.WriteLine(data);
+            int[,] matrix1 = new int[3, 4];
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    matrix1[i, j] = int.Parse(data[i][j]);
+                }
+            }
+
+         
+            for ( int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for ( int j =0; j < matrix1.GetLength(1); j++)
+                {
+                    Console.Write(matrix1[i, j]+ " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("sum matrix :" + newfile.SumMatrix(matrix1));
+            int dem = 0;
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    if (newfile.CheckSNT(matrix1[i, j]))
+                    {
+                        dem++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"số lượng số lượng nguyên tố : {dem}");
+            int Count = 0;
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    if (matrix1[i,j] %2 !=0)
+                    {
+                        Count++;
+                    }
+                }
+            }
+            Console.WriteLine($"số lẽ trong ma trận : { Count}");
+
+            Console.WriteLine($"tổng gia tri duong bien : {newfile.borderline(matrix1)}");
+            Console.WriteLine("hien thi ma tran nhan 3 :");
+
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                   
+                    Console.Write(matrix1[i, j]*3 + "\t");
+                }
+                Console.WriteLine();
+            }
+            //newfile.sum2so(4, 5);
+
+          
+
+
+            using (StreamWriter sw = File.CreateText(output))
+            {
+                sw.WriteLine("tong gia tri ma tran: " + newfile.SumMatrix(matrix1));
+                sw.WriteLine("so luong so nguyen to trong ma tran: " + dem );
+                sw.WriteLine("so luong so le trong ma tran: " + Count );
+                sw.WriteLine("tổng đường biên : " + newfile.borderline(matrix1));
+                for (int i = 0; i < matrix1.GetLength(0); i++)
+                {
+                   
+                    for (int j = 0; j < matrix1.GetLength(1); j++)
+                    {
+
+                        sw.Write(matrix1[i, j] * 3 + "\t");
+                        
+                    }
+                    sw.WriteLine();
+                }
+                Console.WriteLine();
+
+
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string path = @"C:\Users\Admin\Desktop\demo\bai-tap-file\InputData.txt";
+//StreamReader sr = File.OpenText(path);
+//string s;
+//while ((s = sr.ReadLine()) != null)
+//{
+//    Console.WriteLine(s);
+//}
+
+
+
+
+//int r = 3;
+//int c = 4;
+//file newfile = new file();
+//var arr = newfile.matrix(r, c);
+//for(int i = 0; i<arr.getlength(0); i++)
+//{
+//    for(int j = 0; j < arr.getlength(1); j++)
+//    {
+//        console.write(arr[i, j]+" ");
+//    }
+//    console.writeline();
+//}
